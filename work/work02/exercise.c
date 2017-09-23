e5d574c81580fd4116c2729d6eb6aa30dff0602e
MUHAMMAD ZAINI DAHLAN A - 1706106841 <muhammad.zaini71@badak.cs.ui.ac.id>
2017-09-23 09:25:09 +0700

    week02

---
+++ work/work02/exercise.c
@@ -0,0 +1,19 @@
+/*
+ * Name: MUHAMMAD ZAINI DAHLAN A
+ * NPM:  1706106841
+ * Class: EXTENSI
+ * Comment: BELAJAR OS ASYIK
+*/
+
+#define LOOP 2 //define me!
+#include <stdio.h>
+
+void main() {
+int input =5; //define me!
+int aa, Hasil;
+	for (aa=0; aa<1; aa++){
+	Hasil=input*input;
+	printf("%d to the power of %d value equals %d\n",input,LOOP,Hasil); 
+	}	
+	// TO DO implement me!!
+}
